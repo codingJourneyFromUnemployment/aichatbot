f60bad6e67d14a5cf370fa38e2044d485e9859b8
codingJourneyFromUnemployment <kwg6qlyv@hotmail.com>
2024-08-18 16:20:49 +0800

    disable test route

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,24 +1,24 @@
-export const runtime = "edge";
+// export const runtime = "edge";
 
-import { NextResponse } from "next/server";
-import { dataService } from "@/services/dataService";
+// import { NextResponse } from "next/server";
+// import { dataService } from "@/services/dataService";
 
-export async function POST(req: Request) {
-  try {
-    const { dolphinKey, userMessage } = await req.json();
+// export async function POST(req: Request) {
+//   try {
+//     const { dolphinKey, userMessage } = await req.json();
 
-    console.log("userMessage:", userMessage);
-    console.log("start fetching role setup");
-    const reply = await dataService.fetchRoleSetup(dolphinKey, userMessage);
+//     console.log("userMessage:", userMessage);
+//     console.log("start fetching role setup");
+//     const reply = await dataService.fetchRoleSetup(dolphinKey, userMessage);
 
-    return NextResponse.json(reply);
-  } catch (error) {
-    console.error("Error fetching role setup:", error);
+//     return NextResponse.json(reply);
+//   } catch (error) {
+//     console.error("Error fetching role setup:", error);
 
-    // 返回一个错误响应
-    return NextResponse.json(
-      { error: "An error occurred while processing your request" },
-      { status: 500 }
-    );
-  }
-}
+//     // 返回一个错误响应
+//     return NextResponse.json(
+//       { error: "An error occurred while processing your request" },
+//       { status: 500 }
+//     );
+//   }
+// }
